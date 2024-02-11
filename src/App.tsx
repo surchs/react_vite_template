1b3e04231d64450d2b017d2ea251f1306cdc2b2e
Sebastian Urchs <sebastian.urchs@gmail.com>
2024-02-10 19:13:34 -0500

    Installed Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from './assets/vite.svg';
-import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -26,6 +25,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <h1 className="bg-red-600 text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
